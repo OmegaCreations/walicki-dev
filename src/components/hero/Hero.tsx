38cf50ad159687bb01fad3172d58007283503cf5
Maksymilian Walicki <mwalicki@student.agh.edu.pl>
2024-01-27 17:22:37 +0100

    Fix - overflow hidden cropping sphere

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,9 +3,7 @@ import style from "./hero.module.css";
 // Hero
 const Hero: React.FC = () => {
   return (
-    <div
-      className={`${style.navBar} flex flex-col justify-center gap-16 overflow-hidden`}
-    >
+    <div className={`${style.navBar} flex flex-col justify-center gap-16`}>
       <div className="text-center">
         <h1 className="text-[2.5em] sm:text-[3em] font-black tracking-widest">
           Walicki Development
